f8d53d66bc0f41bed27f5c28dee62c6a874732ff
nwshane <django09@gmail.com>
2019-12-10 10:31:10 -0500

    Added reflections for each article

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import { groupBy } from "lodash";
+
 import Head from "../components/head";
 import articles from "../sheetData/articles";
+import reflections from "../sheetData/reflections";
 import { formatDate } from "../helpers";
+
+const reflectionsByArticle = groupBy(reflections, "article");
 
 export default () => (
   <div>
@@ -10,6 +15,15 @@ export default () => (
         <div key={`${article.title}${article.notes}`}>
           <h2>{article.title}</h2>
           <p>{formatDate(article.date)}</p>
+          <ul>
+            {reflectionsByArticle[article.title] &&
+              reflectionsByArticle[article.title].map(reflection => (
+                <div>
+                  <div>{reflection.reflection}</div>
+                  <p>- {reflection.name}</p>
+                </div>
+              ))}
+          </ul>
         </div>
       ))}
     </ul>

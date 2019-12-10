758b72448f4152601d0c71c5b0c084fd4cd777c7
nwshane <django09@gmail.com>
2019-12-10 10:39:46 -0500

    Show more article info

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,19 @@ export default () => (
     <ul>
       {articles.map(article => (
         <div key={`${article.title}${article.notes}`}>
-          <h2>{article.title}</h2>
+          <h2>
+            <a href={article.url || "#"} target="_blank">
+              {article.title}
+            </a>
+          </h2>
+          <p>{article.mediaCompany}</p>
           <p>{formatDate(article.date)}</p>
+          {article.excerpt && <div>Excerpt: {article.excerpt}</div>}
           <ul>
             {reflectionsByArticle[article.title] &&
               reflectionsByArticle[article.title].map(reflection => (
                 <div>
+                  <h3>Reflections</h3>
                   <div>{reflection.reflection}</div>
                   <p>- {reflection.name}</p>
                 </div>

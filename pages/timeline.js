cb801d486fef8fa3a041cd56a9d0d7d5cd48e2dd
nwshane <django09@gmail.com>
2019-12-15 16:54:40 -0500

    Added articles to timeline

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,12 +1,47 @@
+import { sortBy } from "lodash";
+
 import Head from "../components/head";
 import Layout from "../components/Layout";
+import { formatDate } from "../helpers";
+import articles from "../sheetData/articles";
+
+const orderedArticles = sortBy(articles, article => new Date(article.date));
 
 export default () => {
   return (
     <Layout>
-      <Head title="Timeline of all Articles" />
+      <Head title="Timeline" />
 
-      <h1>Timeline of all Articles</h1>
+      <h1>Scott Shane's Articles over Time</h1>
+
+      {orderedArticles.map(article => (
+        <div className="container">
+          <p className="line-above-title">
+            {formatDate(article.date) || "No Date"} | {article.mediaCompany}
+          </p>
+          <h2 className="title">
+            <a href={article.url || "#"} target="_blank">
+              {article.title}
+            </a>
+          </h2>
+          <style jsx>{`
+            .container {
+              margin-bottom: 20px;
+              list-style-type: none;
+              padding-left: 0;
+            }
+            .line-above-title {
+              font-size: 13px;
+              margin-bottom: -5px;
+            }
+            .title {
+              font-size: 24px;
+              margin: 0;
+              text-decoration: underline;
+            }
+          `}</style>
+        </div>
+      ))}
     </Layout>
   );
 };

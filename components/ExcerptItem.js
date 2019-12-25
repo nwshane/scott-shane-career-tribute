f12b64a53cf62675b4d919d6502beebfc70fb88b
nwshane <django09@gmail.com>
2019-12-24 22:54:43 -0500

    Add showFull option

--- components/ExcerptItem.js
+++ components/ExcerptItem.js
@@ -1,18 +1,39 @@
+import { useState } from "react";
+
 export default ({ excerpt }) => {
+  // const [showFull, setCount] = useState(0);
+  const showFull = true;
+
+  const preparedText = excerpt
+    .split("\n")
+    .filter(paragraph => paragraph.trim() !== "");
+
   return (
-    <div className="excerpt">
-      {excerpt
-        .split("\n")
-        .filter(paragraph => paragraph.trim() !== "")
-        .map((paragraph, paragraphIndex) => (
-          <p className="paragraph" key={paragraphIndex}>
-            {paragraph}
-          </p>
-        ))}
+    <>
+      {showFull ? (
+        <div className="excerpt">
+          {preparedText.map((paragraph, paragraphIndex) => (
+            <p className="paragraph" key={paragraphIndex}>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+      ) : (
+        <div className="excerpt">
+          <p className="paragraph ellipsis">{preparedText[0]}</p>
+        </div>
+      )}
       <style jsx>{`
         .excerpt {
           margin-top: 30px;
           position: relative;
+        }
+        .ellipsis {
+          height: 2em;
+          max-width: 90%;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
         .excerpt:before {
           position: absolute;
@@ -26,6 +47,6 @@ export default ({ excerpt }) => {
           margin-bottom: 15px;
         }
       `}</style>
-    </div>
+    </>
   );
 };
